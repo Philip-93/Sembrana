14d8a5e3a8ba9baec22bf793c5523fc3ef0334c6
Philip <philip@Philips-MacBook-Pro-2.local>
2020-03-10 16:07:43 -0700

    my first commit

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <div className="nav">
+      <header className="nav__main-container">
+        <div className="nav-container"></div>
+      </header>
+    </div>
+  );
+}
+
+export default Navigation;
