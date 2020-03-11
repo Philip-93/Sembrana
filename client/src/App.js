77d104a46ecd8b879051920ef3c47d3e47e95d98
Philip <philip@Philips-MacBook-Pro-2.local>
2020-03-11 14:08:39 -0700

    Added images, fonts, illustrations and logos to assets folder and made first iteration of header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import Navigation from "./components/Navigation";
 import "./styles/main.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <Router>
+        <Navigation />
+      </Router>
+    );
   }
 }
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,10 +1,38 @@
+import logo from "../assets/Logo/basicLogoWhite.svg";
+import logopng from "../assets/Logo/basicLogoWhite.png";
+import { Link } from "react-router-dom";
 import React from "react";
 
 function Navigation() {
   return (
     <div className="nav">
       <header className="nav__main-container">
-        <div className="nav-container"></div>
+        <div className="nav__logo-container">
+          <Link to="/">
+            <img
+              className="nav__logo-container--logo"
+              src={logopng}
+              alt="Sembrana"
+            />
+          </Link>
+        </div>
+        <div className="nav__pages-container">
+          <div className="nav__servicios-container">
+            <Link to="/servicios" className="nav__link">
+              <h4 className="nav__title">Servicios</h4>
+            </Link>
+          </div>
+          <div className="nav__productos-container">
+            <Link to="productos" className="nav__link">
+              <h4 className="nav__title">Productos</h4>
+            </Link>
+          </div>
+          <div className="nav__nuestra-historia-container">
+            <Link to="/nuestra-historia" className="nav__link">
+              <h4 className="nav__title">Nuestra Historia</h4>
+            </Link>
+          </div>
+        </div>
       </header>
     </div>
   );
